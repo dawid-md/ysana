fa3a7b80b90900d82c208c7bd11f52bd9e34cbf0
Dawid Mądrzyk <maddawid@gmail.com>
2023-02-11 02:18:31 +0100

    login style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -49,19 +49,21 @@ export default function Login(){
     }
 
     return(
-        <>
-            <h2>Sign In</h2>
+        <div>
+            <h2 className="text-center">Sign In</h2>
             <form>
-            <div className="mb-3 col-3">
+            <div className="mb-3 col-3 mx-auto">
                 <label className="form-label">Email address</label>
                 <input onChange={changeCredentials} name="email" type="email" className="form-control" />
             </div>
-            <div className="mb-3 col-3">
+            <div className="mb-3 col-3 mx-auto">
                 <label className="form-label">Password</label>
                 <input onChange={changeCredentials} name="password" type="password" className="form-control"/>
             </div>
-            <button onClick={submit} type="submit" className="btn btn-primary">Submit</button>
+            <div className="text-center">
+                <button onClick={submit} type="submit" className="btn btn-primary btn-lg mx-22">Submit</button>
+            </div>
             </form>
-        </>
+        </div>
     )
 }
