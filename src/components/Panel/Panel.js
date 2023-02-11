1e6cac6e15fbea6d706046cfa4f1f9e39561abc1
Dawid Mądrzyk <maddawid@gmail.com>
2023-02-11 01:54:23 +0100

    panel changes

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import AuthContext from "../../context/AuthContext"
 
 export default function Panel(){
+    const { isAuthenticated } = useContext(AuthContext)
+
     return(
         <div className="left-panel d-flex">
             <nav className="nav flex-column mt-4">
             <Link to={"/"}>
                 <span className="nav-link">Home</span>
             </Link>
-            <Link to={"/login"}>
-                <span className="nav-link">Login</span>
+            <Link to={"/"}>
+                <span className="nav-link">Settings</span>
             </Link>
-            <Link to={"/register"}>
-                <span className="nav-link">Register</span>
-            </Link>
+            {!isAuthenticated ? 
+            <div>
+                <Link to={"/login"}>
+                    <span className="nav-link">Login</span>
+                </Link>
+                <Link to={"/register"}>
+                    <span className="nav-link">Register</span>
+                </Link>
+            </div> : null
+            }
+
             </nav>
         </div>
     )
