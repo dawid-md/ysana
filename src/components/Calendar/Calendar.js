fc2dfc2a43f270da492e8a87692263f4660edb42
Dawid Mądrzyk <maddawid@gmail.com>
2023-04-14 01:06:24 +0200

    calendar shows number of tasks per each day

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,16 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from "../../context/AuthContext";
+import axios from 'axios';
 
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
+  const [tasks, setTasks] = useState([])
+  const [tasksByDate, settasksByDate] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { isAuthenticated, currentUser } = useContext(AuthContext)
+
+  async function getData(){
+    const resTasks = await axios.get(`https://ysana-d79f4-default-rtdb.europe-west1.firebasedatabase.app/tasks.json?auth=${currentUser.token}`)
+    const tasksData = []
+    const tasksByDateCounter = []
+    for(const key in resTasks.data){
+        tasksData.push({...resTasks.data[key], id: key})
+        if(tasksByDateCounter[resTasks.data[key].duedate] == undefined){tasksByDateCounter[resTasks.data[key].duedate] = 0}
+        tasksByDateCounter[resTasks.data[key].duedate] += 1
+    }
+    //console.log(tasksByDateCounter);
+    setTasks(tasksData)
+    settasksByDate(tasksByDateCounter)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if(isAuthenticated && currentUser.token) {
+        getData()
+    }
+    else if(isAuthenticated === false){
+        //navigate("/login")
+    }
+}, [isAuthenticated, currentUser])
+
+  const getCurrentDateFormatted = () => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so we add 1 and pad with a leading 0 if needed
+    const day = String(d.getDate()).padStart(2, '0'); // Pad with a leading 0 if needed
+    return `${year}-${month}-${day}`;
+  }
 
   const generateMatrix = () => {
     const year = date.getFullYear();
     const month = date.getMonth();
   
-    const firstDay = new Date(year, month, 1).getDay()-1;         //day of week with mon as the first one
+    const firstDay = new Date(year, month, 0).getDay();           //day of week with mon as the first one
     const daysInMonth = new Date(year, month + 1, 0).getDate();   //last day of current month
-
-    console.log(daysInMonth);
   
     const matrix = [];
     let counter = 1;
@@ -20,6 +56,9 @@ const Calendar = () => {
       for (let col = 0; col < 7; col++) {
         if (row === 0 && col < firstDay) {
           matrix[row][col] = null;
+        } else if (counter > daysInMonth && col === 0) {
+          matrix.splice(row)
+          break
         } else if (counter > daysInMonth) {
           matrix[row][col] = null;
         } else {
@@ -28,7 +67,7 @@ const Calendar = () => {
         }
       }
     }
-    console.log(matrix);
+    //console.log(matrix);
     return matrix;
   };
   
@@ -38,15 +77,20 @@ const Calendar = () => {
   ];
   
   const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  let xDate = getCurrentDateFormatted()
+  //console.log(xDate.substring(0,8) + 22);
+  //console.log(tasksByDate['2023-03-17']);
+  //console.log(tasksByDate[(xDate.substring(0,8) + 22)]);
   
   return (
+    !loading ? 
     <div>
       <h2>
-        <button onClick={() => setDate(new Date(date.setMonth(date.getMonth() - 1)))}>Prev</button>
+        <button className='btn btn-light border-secondary mx-3 my-3' onClick={() => setDate(new Date(date.setMonth(date.getMonth() - 1)))}>Prev</button>
         {monthNames[date.getMonth()]} {date.getFullYear()}
-        <button onClick={() => setDate(new Date(date.setMonth(date.getMonth() + 1)))}>Next</button>
+        <button className='btn btn-light border-secondary mx-3 my-3' onClick={() => setDate(new Date(date.setMonth(date.getMonth() + 1)))}>Next</button>
       </h2>
-      <table>
+      <table className="calendar">
         <thead>
           <tr>
             {dayNames.map(day => (
@@ -58,13 +102,14 @@ const Calendar = () => {
           {generateMatrix().map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
-                <td key={cellIndex}>{cell}</td>
+                <td key={cellIndex}><span>{cell}</span><span className='text-right'>{tasksByDate[xDate.substring(0,8) + ("0" + cell).slice(-2)]}</span></td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+    : null
   );
 };
 
